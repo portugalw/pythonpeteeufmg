4f93143e9ceae255f5d237ee4bd783c4d23e91a6
Washington <portugalw@gmail.com>
2023-05-23 22:17:25 -0300

    Iniciando aulas 05 e 06

---
+++ 05_imports.py
@@ -0,0 +1,11 @@
+from math import cos, pi, sqrt
+from pandas import *
+
+
+_cos = cos(pi)
+
+print(_cos)
+
+sqrtX = sqrt(25)
+
+print(dir())
---
+++ 06_entrada_dados.py
@@ -0,0 +1,3 @@
+idade = int(input("Digite sua idade: "))
+
+print("Sua idade é:" , idade, '\nO nome é Washington', sep='---', end=';') #convert int para string, com +nao converte
